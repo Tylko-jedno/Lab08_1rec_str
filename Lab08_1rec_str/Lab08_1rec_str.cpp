ba5ead0c571e39ded8c765967ec54b98e3268039
Tylko-jedno <diogkek@gmail.com>
2024-10-22 15:33:02 +0300

    first master commit

---
+++ Lab08_1rec_str/Lab08_1rec_str.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Count(const string& str, int index = 0) {
+    if (index == str.length()) {
+        return 0;
+    }
+    int count = (str[index] == '+' || str[index] == '-' || str[index] == '=') ? 1 : 0;
+    return count + Count(str, index + 1);
+}
+
+int CalculateNewLength(const string& str, int index = 0) {
+    if (index == str.length()) {
+        return 0;
+    }
+
+    if (str[index] == '+' || str[index] == '-' || str[index] == '=') {
+        return 2 + CalculateNewLength(str, index + 1);
+    }
+    else {
+        return 1 + CalculateNewLength(str, index + 1);
+    }
+}
+
+void ChangeHelper(const string& str, string& newStr, int index = 0) {
+    if (index == str.length()) {
+        return;
+    }
+
+    if (str[index] == '+' || str[index] == '-' || str[index] == '=') {
+        newStr += "**";
+    }
+    else {
+        newStr += str[index];
+    }
+
+    ChangeHelper(str, newStr, index + 1);
+}
+
+string Change(const string& str) {
+    string newStr;
+    ChangeHelper(str, newStr);
+    return newStr;
+}
+
+int main()
+{
+    string str;
+    cout << "Enter string:" << endl;
+    getline(cin, str);
+
+    cout << "String contained " << Count(str) << " symbols: +-=" << endl;
+
+    string result = Change(str);
+    cout << "Modified string (result): " << result << endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <string>
+#include "../Lab08_1rec_str/Lab08_1rec_str.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestCount)
+        {
+            std::string str1 = "a+b-c=d";
+
+            Assert::AreEqual(3, Count(str1));
+        }
+
+        TEST_METHOD(TestChange)
+        {
+            std::string str1 = "a+b-c=d";
+            std::string expected1 = "a**b**c**d";
+
+            Assert::AreEqual(expected1, Change(str1));
+        }
+    };
+}
+
